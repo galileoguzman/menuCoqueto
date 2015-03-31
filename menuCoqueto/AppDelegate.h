961cb1b3d838f1aae875a07c7a26fe3e0a115043
Galileo Guzmán <galileoguzman@gmail.com>
2015-03-30 22:58:22 -0600

    Initial Commit

---
+++ menuCoqueto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  menuCoqueto
+//
+//  Created by Galileo Guzman on 30/03/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ menuCoqueto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  menuCoqueto
+//
+//  Created by Galileo Guzman on 30/03/15.
+//  Copyright (c) 2015 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
